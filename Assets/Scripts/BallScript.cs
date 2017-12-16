9c80775c3637e72a8094e1e39b5d9fc5d76503b5
Rodrigo Cocate <rodrigo.cocate@hotmail.com>
2017-12-16 01:37:56 -0200

    Primeiro commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float velocidade;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * velocidade;
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.name == "baseblock")
+        {
+            // Calculate posição onde bola acertou base.
+            float x = AcertouBase(transform.position, c.transform.position, c.collider.bounds.size.x);
+
+            // Calculate direção e seta tamanho pra 1.
+            Vector2 direcao = new Vector2(x, 1).normalized;
+
+            // Redefine direção da bola.
+            GetComponent<Rigidbody2D>().velocity = direcao * velocidade;
+        }
+    }
+
+    float AcertouBase(Vector2 bolaPosicao, Vector2 basePosicao, float tamanhoBase)
+    {
+        /*
+         *  1  -0.5  0  0.5   1  <- x value
+         *  ===================  <- baseBlock
+         */
+        return ((bolaPosicao.x - basePosicao.x) / tamanhoBase);
+    }
+
+}
---
+++ Assets/Scripts/BaseBlockScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BaseBlockScript : MonoBehaviour {
+
+    public float velocidade;
+    public float limiteEsquerdo, limiteDireito;
+    
+	// Update is called once per frame
+	void Update () {
+        Mover();
+    }
+
+    void Mover()
+    {
+
+        // Mover
+        var move_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+        transform.Translate(move_x, 0.0f, 0.0f);
+
+        // Wrap
+        if (transform.position.x < limiteEsquerdo || transform.position.y > limiteDireito)
+        {
+            transform.position = new Vector2(transform.position.x * -1, transform.position.y);
+        }
+    }
+}
